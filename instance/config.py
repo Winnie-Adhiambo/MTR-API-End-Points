65f641b0e7ffb065515d3a56fab8940ebbabf724
AMOSMWORIA <amosmworia007@gmail.com>
2018-05-30 21:29:34 +0300

    [chore-#157970769]added environment config

---
+++ instance/config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config(object):
+""""Parent Configuration Class"""
+DEBUG = False
+CSFR_ENABLED = True
+SECRET = os.getenv('SECRET')
+
+
+class DevelopmentConfig(Config):
+"""Configuration for development"""
+DEBUG = True
+
+
+class TestingConfig(Config)
+"""Configuration for testing separate database"""
+TESTING = True
+DEBUG=True
+
+class StagingConfig(Config):
+"""Configuration for staging"""
+DEBUG = True
+
+class productionconfig(Config)
+""" Configuration for production"""
+DEBUG = False
+TESTING False
+
+app_config = {
+
+'development':DevelopmentConfig,
+'testing': TestingConfig,
+'staging': StagingConfig,
+'production': ProductionConfig
+
+}
