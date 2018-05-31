4baede7e1b7f452197cd55aad60426a69507bf6a
AMOSMWORIA <amosmworia007@gmail.com>
2018-05-31 23:42:27 +0300

    [Feature #157968435] contains general settings for environment

--- instance/config.py
+++ instance/config.py
@@ -1,30 +1,30 @@
 import os
 
 class Config(object):
-""""Parent Configuration Class"""
-DEBUG = False
-CSFR_ENABLED = True
-SECRET = os.getenv('SECRET')
+    """Parent Configuration Class"""
+    DEBUG = False
+    CSFR_ENABLED = True
+    SECRET = os.getenv('SECRET')
 
 
 class DevelopmentConfig(Config):
-"""Configuration for development"""
-DEBUG = True
+    """Configuration for development"""
+    DEBUG = True
 
 
-class TestingConfig(Config)
-"""Configuration for testing separate database"""
-TESTING = True
-DEBUG=True
+class TestingConfig(Config):
+    """Configuration for testing separate database"""
+    TESTING = True
+    DEBUG=True
 
 class StagingConfig(Config):
-"""Configuration for staging"""
-DEBUG = True
+    """Configuration for staging"""
+    DEBUG = True
 
-class productionconfig(Config)
-""" Configuration for production"""
-DEBUG = False
-TESTING False
+class ProductionConfig(Config):
+    """ Configuration for production"""
+    DEBUG = False
+    TESTING = False
 
 app_config = {
 
