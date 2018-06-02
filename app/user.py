77c94e103baf3eaf70e117e2e9c9b716a9e53f48
AMOSMWORIA <amosmworia007@gmail.com>
2018-06-03 00:43:31 +0300

    [ft-userlogin-#157968435]updated requirements for userlogin

---
+++ app/user.py
@@ -0,0 +1,6 @@
+# class user
+class User():
+    def __init__(self,username,password,confirmpassword):
+        self.username=username
+        self.password=password
+        self.confirmpassword=confirmpassword
